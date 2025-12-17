58169656b827e49364809ce23112a7e5cf5c912a
Thiry Prune <prune.thiry@hotmail.com>
2025-12-17 10:55:05 +0100

    style: add comments

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { User } from 'types/usersType';
 
 @Injectable()
 export class UsersService {
+    // simulate users db
     private users: User[] = [
         {
             id: '1', 
@@ -36,15 +37,19 @@ export class UsersService {
         }
     ]
 
+    // return all users from db
     findAll(): User[] {
         return this.users;
     } 
 
+    // return one user discriminated by id from db
     findeOne(id: string): User {
         return this.users.find((user) => user.id === id) as User;
     }
 
+    // add a new user into db, return the newly created user 
     create(user: User): User {
+        // since id isn't autoincremented yet, do it manually
         const newId = (this.users.length + 1).toString();
         const newUser = {
             ...user, 
@@ -54,12 +59,15 @@ export class UsersService {
         return newUser;
     }
 
+    // update an existing user, return the updated user
     update(id: String, user: User): User {
+        // discriminate by id
         const index = this.users.findIndex((user) => user.id === id);
         this.users[index] = user;
         return user;
     }
 
+    // delete an existing user, return a string
     delete(id: String): string {
         this.users = this.users.filter((user) => user.id !== id);
         return "User deleted successfully";
